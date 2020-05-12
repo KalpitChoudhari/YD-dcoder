9cec90bf33d5e4008aedfefaddb4c7d32d7ebea6
Yadynesh-Nandane <44375113+Yadynesh-Nandane@users.noreply.github.com>
2020-05-12 10:03:33 +0530

    Update Happy_String.py

--- Happy_String.py
+++ Happy_String.py
@@ -1,4 +1,8 @@
- 
+"""
+Author: Yadynesh-Nandane
+Program: Happy_String
+"""
+
 #declaring a dictionary with the number as key and letter as value
 d= {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g',8:'h',
 9:'i',10:'j',11:'k',12:'l',13:'m',14:'n',15:'o',
